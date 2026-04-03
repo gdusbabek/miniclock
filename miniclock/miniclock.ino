1491f4c9aabd53d65f20d22fc198a9f09ae6f861
gdusbabek <gdusbabek@gmail.com>
2026-04-03 05:59:34 -0500

    Adjust the divider position and thickness
    - move the horizontal rule up by one pixel
    - render the rule as a two-pixel-thick bar instead of a single-pixel line

--- miniclock/miniclock.ino
+++ miniclock/miniclock.ino
@@ -239,7 +239,7 @@ void drawDisplayHeader(const char* timeLine,
                        const GFXfont* dateFont) {
   drawCenteredText(timeLine, timeFont, display.width() / 2, 24);
   drawCenteredText(dateLine, dateFont, display.width() / 2, 59);
-  display.drawLine(7, 76, display.width() - 8, 76, GxEPD_BLACK);
+  display.fillRect(8, 75, display.width() - 16, 2, GxEPD_BLACK);
 }
 
 void drawDisplayLocationBlock(const char* locator,
